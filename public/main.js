f1a67f53a734545a1484ac32fa94573bba848b25
Naomi Nazar <naomi.nazar@hva.nl>
2018-02-08 16:53:21 +0100

    Formatted code to code convention

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
+/** This function changes the color of the button */
 function changeColor() {
-    document.getElementById("test").style.backgroundColor = "pink";
+  document.getElementById('test').style.backgroundColor = 'pink';
 }
+
 module.exports = {changeColor};
--- test/test.js
+++ test/test.js
@@ -1,15 +1,14 @@
 (() => {
-    'use strict';
-var expect = require('chai').expect;
-var request = require('request');
+  'use strict';
+  const expect = require('chai').expect;
+  const request = require('request');
 
-describe('test', function () {
-    it('should change color', function () {
-        request('http://localhost:63342', function (error, response, body) {
-            expect(body).to.equal('Test Page');
-            done();
-        });
+  describe('test', function() {
+    it('should change color', function() {
+      request('http://localhost:63342', function(error, response, body) {
+        expect(body).to.equal('Test Page');
+        expect.done();
+      });
     });
-});
-})
-();
+  });
+})();
