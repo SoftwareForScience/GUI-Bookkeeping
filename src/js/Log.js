1c688e189e78a14dd05edf5404bdc73d7f4c617a
Naomi Nazar <naomi.nazar@hva.nl>
2018-06-07 10:20:08 +0200

    Changed navigation bar to hamburger menu

--- src/js/Log.js
+++ src/js/Log.js
@@ -5,7 +5,7 @@ let Log = {
   loadList: function() {
     return m.request({
       method: 'GET',
-      url: 'http://localhost:8290/api/all/entries?personid=0&name=Anonymous&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwidXNlcm5hbWUiOiJBbm9ueW1vdXMiLCJhY2Nlc3MiOjAsImlhdCI6MTUyODE4OTgwMiwiZXhwIjoxNTI4Mjc2MjAyLCJpc3MiOiJvMi11aSJ9.jjpQLVwGXHemgHFseGvJFTPhWr4tg0TKFYb1OBwRE3w'
+      url: 'http://localhost:8290/api/all/entries?personid=0&name=Anonymous&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwidXNlcm5hbWUiOiJBbm9ueW1vdXMiLCJhY2Nlc3MiOjAsImlhdCI6MTUyODI4NTA2NywiZXhwIjoxNTI4MzcxNDY3LCJpc3MiOiJvMi11aSJ9.jnPQCWV60LAhcpyKzzEwjUh0Q-6InUOW_FSMMB8xP7o'
       // withCredentials: true
     });
     // .then(function(data) {
@@ -19,7 +19,7 @@ let Log = {
     return m.request({
       method: 'POST',
       /* eslint max-len: ["error", { "ignoreUrls": true }]*/
-      url: 'http://localhost:8290/api/post/entry/data?personid=0&name=Anonymous&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwidXNlcm5hbWUiOiJBbm9ueW1vdXMiLCJhY2Nlc3MiOjAsImlhdCI6MTUyODE4OTgwMiwiZXhwIjoxNTI4Mjc2MjAyLCJpc3MiOiJvMi11aSJ9.jjpQLVwGXHemgHFseGvJFTPhWr4tg0TKFYb1OBwRE3w',
+      url: 'http://localhost:8290/api/post/entry/data?personid=0&name=Anonymous&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwidXNlcm5hbWUiOiJBbm9ueW1vdXMiLCJhY2Nlc3MiOjAsImlhdCI6MTUyODI4NTA2NywiZXhwIjoxNTI4MzcxNDY3LCJpc3MiOiJvMi11aSJ9.jnPQCWV60LAhcpyKzzEwjUh0Q-6InUOW_FSMMB8xP7o',
       //   headers: {
       //       'Content-Type' : 'application/json'
       //   },
@@ -42,7 +42,7 @@ let Log = {
   getSingle: function(id) {
     return m.request({
       method: 'GET',
-      url: 'http://localhost:8290/api/single/entry/' + id + '?personid=0&name=Anonymous&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwidXNlcm5hbWUiOiJBbm9ueW1vdXMiLCJhY2Nlc3MiOjAsImlhdCI6MTUyODE4OTgwMiwiZXhwIjoxNTI4Mjc2MjAyLCJpc3MiOiJvMi11aSJ9.jjpQLVwGXHemgHFseGvJFTPhWr4tg0TKFYb1OBwRE3w'
+      url: 'http://localhost:8290/api/single/entry/' + id + '?personid=0&name=Anonymous&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwidXNlcm5hbWUiOiJBbm9ueW1vdXMiLCJhY2Nlc3MiOjAsImlhdCI6MTUyODI4NTA2NywiZXhwIjoxNTI4MzcxNDY3LCJpc3MiOiJvMi11aSJ9.jnPQCWV60LAhcpyKzzEwjUh0Q-6InUOW_FSMMB8xP7o'
     });
   }
 };
--- src/js/NavBar.js
+++ src/js/NavBar.js
@@ -1,33 +1,50 @@
 const m = require('mithril');
 
 module.exports = {
-  oncreate: dontShow,
+  // oncreate: dontShow,
   view: function() {
     return [
-      m('nav.navbar.navbar-default [id = navigation][style=background-color: #ef8239;]', [
-        m('div.container-fluid', [
-          m('div.navbar-header', [
-            m('a.navbar-brand', {href: '#!/welcomeScreen'}, 'Alice')
-          ]),
-          m('ul.nav navbar-nav', [
-            m('li', m('a', {href: '#!/logList'}, 'Log list')),
-            m('li', m('a', {href: '#!/createLog'}, 'Create log')),
-            m('li', m('a', {href: '#!/fileUpload'}, 'File upload')),
-            m('li', m('a', {href: '#!/view', id: 'hello'}, 'Move'))
+      /* eslint max-len: ["error", { "ignoreStrings": true }]*/
+      m('nav[class= navbar navbar-expand-lg navbar-light bg-light][id = navigation][style = background-color: #ef8239 !important;]', [
+        m('a[class= navbar-brand]', {href: '#!/welcomeScreen'}, [
+        ], 'ALICE'),
+        m('button[class = navbar-toggler][type=button][data-toggle=collapse][data-target=#navbarSupportedContent][aria-controls=navbarSupportedContent][aria-expanded=false][aria-label=Toggle navigation]', [
+          m('span[class= navbar-toggler-icon]')
+        ]),
+        m('div[class=collapse navbar-collapse][id=navbarSupportedContent]', [
+          m('ul[class= navbar-nav mr-auto]', [
+            m('li[class=nav-item active]', [
+              m('a[class= nav-link]', {href: '#!/welcomeScreen'}, [
+                m('span[class= sr-only]', '(current)')
+              ], 'Home')
+            ]),
+            m('li[class= nav-item]', [
+              m('a[class= nav-link]', {href: '#!/logList'}, 'Log list')
+            ]),
+            m('li[class= nav-item]', [
+              m('a[class= nav-link]', {href: '#!/createLog'}, 'Create log')
+            ]),
+            m('li[class= nav-item]', [
+              m('a[class= nav-link]', {href: '#!/fileUpload'}, 'File Upload')
+            ]),
+            m('li[class= nav-item]', [
+              m('a[class= nav-link]', {href: '#!/view'}, 'Move')
+            ])
           ])
         ])
       ])
     ];
   }
 };
-
 /** Doesn't show navbar at the login page (doesn't completely work yet)
  */
-function dontShow() {
-  let nav = document.getElementById('navigation');
-  if (m.route.get === '/login') {
-    nav.style.visibility = 'hidden';
-  } else {
-    nav.style.display = 'visible';
-  }
-}
+// function dontShow() {
+//   console.log('hallo');
+//   let nav = document.getElementById('navigation');
+//   if (m.route.get = '/login') {
+//     nav.style.visibility = 'hidden';
+//   } else {
+//     nav.style.display = 'visible';
+//   }
+// }
+
